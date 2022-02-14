c25700de6fae62282efaf33d4593850d72a98a85
TKadyear <tamarakadyear@gmail.com>
2022-02-14 11:51:48 +0100

    Added user profile from api.github.com
    Fetch in the created because I need the content like
    the DOMContentLoaded of the Javascript addEventListener

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main class="w-3/4 m-auto grid grid-cols-3">
+    <UserProfile
+      :nameUser="user.name"
+      :imageUserProfile="user.avatar_url"
+      :since="user.created_at"
+      :description="user.bio"
+      :numberFriends="user.following"
+    />
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import UserProfile from './components/UserProfile.vue'
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    UserProfile
+  },
+  data() {
+    return {
+      user: {}
+    }
+  },
+  created() {
+    fetch('https://api.github.com/users/mojombo')
+      .then(response => response.json())
+      .then(data => {
+        let fullYear = new Date(data.created_at)
+        fullYear = fullYear.getFullYear()
+        data.created_at = fullYear
+        this.user = data
+      })
   }
 }
 </script>
---
+++ src/components/UserProfile.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <img :src="imageUserProfile" :alt="nameUser" />
+    <p class="user-name">{{ nameUser }}</p>
+    <p class="text-gray-400">Joined in {{ since }}</p>
+    <p>{{ description }}</p>
+    <hr />
+    <p class="text-gray-400">{{ numberFriends }} Friends</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'BoilerPlate',
+  props: {
+    nameUser: { type: String, required: true },
+    imageUserProfile: { type: String, required: true },
+    since: { type: String, required: true },
+    description: { type: String, required: true },
+    numberFriends: { type: String, required: true }
+  }
+}
+</script>
+<style scoped>
+.container {
+  @apply w-80 rounded-xl;
+  box-shadow: 0 4px 8px 4px #00000050;
+  transition: 500ms;
+}
+.container:hover {
+  transform: scale(1.1);
+}
+.container > img {
+  @apply w-80 rounded-t-xl;
+}
+.user-name {
+  @apply font-bold text-xl;
+}
+</style>
